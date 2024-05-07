f1ae242331fe9026c860418d1e2488d2dd00b8a3
starmine7940 <starmine7940@gmail.com>
2024-05-08 01:39:33 +0900

    クリアボタンの追加

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,12 +1,12 @@
 import { createUseStyles } from "react-jss"
-import { FC, ChangeEvent } from "react"
+import { FC, ChangeEvent, useState } from "react"
 import { Header, Form, TextArea, Button, Container } from "semantic-ui-react"
 import "semantic-ui-css/semantic.min.css"
 import React from "react"
 
 const useStyles = createUseStyles({
-  button: {
-    margin: "1em 0em !important",
+  buttonContainer: {
+    margin: "1em 0em",
   },
 })
 
@@ -23,12 +23,19 @@ export const Input: FC<InputProps> = ({
 }) => {
   const classes = useStyles()
 
+  const [canEditTextArea, setCanEditTextArea] = useState<boolean>(true)
+
   const handleInputTextChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
     updateInputTexts(event.target.value)
   }
 
   const handleSubmitButtonClick = async () => {
+    setCanEditTextArea(false)
     await onRequest()
+  }
+
+  const handleClearButtonClick = () => {
+    setCanEditTextArea(true)
   }
 
   return (
@@ -41,15 +48,21 @@ export const Input: FC<InputProps> = ({
           placeholder="テキストを入力してください"
           onChange={handleInputTextChange}
           rows={8}
-          disabled={isFetching}
+          disabled={!canEditTextArea}
         />
       </Form>
-      <Button
-        content="決定"
-        onClick={handleSubmitButtonClick}
-        disabled={isFetching}
-        className={classes.button}
-      />
+      <div className={classes.buttonContainer}>
+        <Button
+          content="決定"
+          onClick={handleSubmitButtonClick}
+          disabled={isFetching}
+        />
+        <Button
+          content="クリア"
+          onClick={handleClearButtonClick}
+          disabled={isFetching}
+        />
+      </div>
     </Container>
   )
 }
